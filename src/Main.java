c19b316958b502f0bc1148187a2515d7a3127247
Logan <mahlerln@mail.uc.edu>
2025-01-31 20:59:00 -0500

    Final Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        Scanner scanner = new Scanner(System.in);
+        double springCost;
+        double summerCost;
+        double fallCost;
+        double winterCost;
+
+
+         System.out.print(" what is the maintenance cost for Spring: ");
+         springCost = scanner.nextDouble();
+
+        System.out.print(" what is the maintenance cost for Summer: ");
+        summerCost = scanner.nextDouble();
+
+
+        System.out.print(" what is the maintenance cost for Fall: ");
+        fallCost = scanner.nextDouble();
+
+
+        System.out.print(" what is the maintenance cost for Winter: ");
+        winterCost = scanner.nextDouble();
+
+double totalCost = springCost + summerCost + fallCost + winterCost;
+
+System.out.printf(" The maintenance cost for spring was: $" + springCost);
+System.out.printf(" The maintenance cost for summer was: $" + summerCost);
+System.out.printf(" The maintenance cost for fall was: $" + fallCost);
+System.out.printf(" The maintenance cost for winter was: $" + winterCost);
+System.out.printf(" The maintenance cost for the year was: $" + totalCost);
+
+scanner.close();
     }
 }
